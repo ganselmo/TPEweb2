6d18cb191b1c718a8e3408493dca91e8beee5596
Geronimo Anselmo <geronimoalejandroanselmo@gmail.com>
2019-10-04 08:52:43 -0300

    Gero Metodos + Handler

--- Repositories/Route.php
+++ Repositories/Route.php
@@ -5,41 +5,50 @@ class Route
 {
 
     private $url;
-    
-    function __construct($action)
+    private $method;
+
+    function __construct()
     {
+
         
-        $this->url = $action;
+        $this->url = explode('/',$_GET['action']);
+
+        $this->method = $_SERVER['REQUEST_METHOD'];
+        $this->seeSession();
         $this->direct();
+    }
+    private function seeSession()
+    {
 
     }
-
     private function direct()
     {
 
-        $json = $this->retriveRoutesJSON();
-        var_dump($json);
-        var_dump ($this->url);
-        if(findMatch())
-        {
+        var_dump($this->url);
 
-        }
-        else{
-            
-        }  
+        $json = $this->retriveRoutesJSON();
+        $this->findMatch($json);
+        $route = $this->findMatch($json);
+
 
     }
 
     private function retriveRoutesJSON()
     {
-        $json =file_get_contents ("Repositories/RouteMap.json");
-        return json_decode($json);  
-        
+        $json = file_get_contents("Repositories/RouteMap.json");
+        return json_decode($json);
     }
 
+    private function findMatch($json)
+    {
 
 
-   
+        foreach ($json->routes as $route) {
 
-   
+            if ($route->point == $this->url && $route->method == $this->method) {
+                return $route;
+            }
+        }
+       
+    }
 }
