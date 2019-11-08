562bdb1c8eb07b2daf03520116292118d0dbb94e
Javier Lopez Ferreira <javierlopez987@gmail.com>
2019-11-08 16:20:31 -0300

    versión de presentación - master

--- Repositories/Scripts/menu.js
+++ Repositories/Scripts/menu.js
@@ -2,18 +2,13 @@ document.addEventListener("DOMContentLoaded",function()
 {
     let btnArtista = document.querySelector("#Artistas");
     let btnCancion = document.querySelector("#Canciones");
-    let btnAC = document.querySelector("#AC");
 
     btnArtista.addEventListener("click",function()
     {
-        window.location=BASE+'Artistas/Get';
+        window.location='Artistas/Get';
     });
     btnCancion.addEventListener("click",function()
     {
-        window.location=BASE+'Canciones/Get';
-    });
-    btnAC.addEventListener("click",function()
-    {
-        window.location=BASE+'Artistas/Canciones';
+        window.location='Canciones/Get';
     });
 });
