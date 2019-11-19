d9529fd3fefc4f6a2dbc54b39a0f46040d109c5f
Geronimo Anselmo <geronimoalejandroanselmo@gmail.com>
2019-11-19 07:26:39 -0300

    Ultimo Commit

--- Repositories/Scripts/menu.js
+++ Repositories/Scripts/menu.js
@@ -2,18 +2,15 @@ document.addEventListener("DOMContentLoaded",function()
 {
     let btnArtista = document.querySelector("#Artistas");
     let btnCancion = document.querySelector("#Canciones");
-    let btnAC = document.querySelector("#AC");
+
 
     btnArtista.addEventListener("click",function()
     {
-        window.location=BASE+'Artistas/Get';
+        window.location='Artistas/Get';
     });
     btnCancion.addEventListener("click",function()
     {
-        window.location=BASE+'Canciones/Get';
+        window.location='Canciones/Get';
     });
-    btnAC.addEventListener("click",function()
-    {
-        window.location=BASE+'Artistas/Canciones';
-    });
+    
 });
